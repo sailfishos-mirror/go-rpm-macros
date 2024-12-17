93e7efaee9759d7e5ea301ecb767e835be5c9c9d
Maxwell G <maxwell@gtmx.me>
2024-12-17 17:51:12 -0600

    dev get_deps: allow passing -b

--- get_deps.sh
+++ get_deps.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
+branch="rhel9"
+
+while getopts 'b:' opt; do
+    case "${opt}" in
+        b)
+            branch="${OPTARG}"
+            ;;
+        *)
+            exit 1
+            ;;
+    esac
+done
+
 podman run --rm \
     -v ~/.cache/fedrq:/fedrq-cache/fedrq:z \
     -ti \
     quay.io/gotmax23/fedrq:ubi9 \
-wr -b rhel9 -r @epel -s -F line:name,repoid:@ go-rpm-macros go-rpm-macros-epel \
+wr -b "${branch}" -r @epel -s -F line:name,repoid:@ go-rpm-macros go-rpm-macros-epel \
     | grep 'epel-source' | sponge | cut -d@ -f1
